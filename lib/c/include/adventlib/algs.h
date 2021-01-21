57ce1c20f00a1f35ca5abf59ea74e1c6c9f5282b
SealabJaster <sealabjaster@gmail.com>
2021-01-21 05:30:37 +0000

    pls just work

--- lib/c/include/adventlib/algs.h
+++ lib/c/include/adventlib/algs.h
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include "types.h"
 
 bool splitAndApply(Slice string, char delim, void* context, bool (*func)(Slice value, void* context));
--- lib/c/include/adventlib/io.h
+++ lib/c/include/adventlib/io.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stddef.h>
 #include "types.h"
 
 bool loadFileAsSlice(char* fileName, Slice* slice);
